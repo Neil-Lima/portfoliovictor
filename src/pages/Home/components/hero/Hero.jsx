bccf642c8e5aec69a874c563a8deca4e865db8eb
Neil-Lima <110132286+Neil-Lima@users.noreply.github.com>
2023-03-02 18:11:46 -0300

    Update Hero.jsx

--- src/pages/Home/components/hero/Hero.jsx
+++ src/pages/Home/components/hero/Hero.jsx
@@ -4,7 +4,7 @@ function Hero() {
   return (
     <>
   <div class="card" style={{background: "url('https://images.unsplash.com/photo-1619410283995-43d9134e7656?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHByb2dyYW1taW5nfGVufDB8fDB8fA%3D%3D&amp;w=1000&amp;q=80')",backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat', padding: "66px"}}>
-    <div class="card-body" style={{background: "rgba(35,35,74,0.81)", textAlign: "center", padding: "46px"}}>
+    <div class="card-body" style={{background: "rgba(35,35,74,0.81)", textAlign: "center", padding: "10px"}}>
       <img class="border  rounded-circle border-5"  src={Victor} style={{width: "150px", border: "6px solid rgb(97,218,251)" }}/>
         <h2 class="card-title" style={{color: "rgb(97,218,251)"}}>VICTOR NEIL</h2>
         <p class="card-text" style={{fontSize: "26px"}}>Desenvolvedor Front-End React</p>
