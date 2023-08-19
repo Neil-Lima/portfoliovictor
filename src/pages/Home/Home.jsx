5c32d6a4828b486a0a6b93bc4887f9dcfd9608bc
Neil-Lima <110132286+Neil-Lima@users.noreply.github.com>
2023-08-19 01:42:25 -0300

    j

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,17 +4,21 @@ import Servicos from './components/servicos/Servicos'
 import Sobre from './components/Sobre/Sobre'
 import Tecnologias from './components/tecnologias/Tecnologias'
 import Navegacao from './components/navegacao/Navegacao'
+import Contato from './components/Contato/Contato'
+import CardsProjetos from './components/CardsProjetos/CardsProjetos'
 
 function Home() {
   return (
-   <>
-   <Navegacao/>
-    <Hero/>
-   <Sobre/><br />
-   <Servicos/> <br /> 
-   <Tecnologias/> 
-   </>
-  
+    <>
+      <Navegacao />
+      <Hero />
+      <Sobre /><br />
+      <Servicos /> <br />
+      <Tecnologias />
+      <CardsProjetos />
+      <Contato />
+    </>
+
   )
 }
 
